049c052a912acbbd5996ad1862d4788b19a22314
Dmitry Kirsanov <dkirsanov@aligntech.com>
2018-03-22 15:28:35 +0300

    improve drone installation instruction

--- install_drone_depends.sh
+++ install_drone_depends.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+sudo apt-get install cmake
+sudo apt-get install libssl-dev
+sudo apt-get install libopencv-dev
+
 rm -rf restbed/
 git clone --recursive https://github.com/corvusoft/restbed.git
 mkdir restbed/build
 cd restbed/build
-cmake [-DBUILD_TESTS=YES] [-DBUILD_EXAMPLES=YES] [-DBUILD_SSL=NO] [-DBUILD_SHARED=YES] [-DCMAKE_INSTALL_PREFIX=/output-directory] ..
-make
+sudo cmake [-DBUILD_TESTS=YES] [-DBUILD_EXAMPLES=YES] [-DBUILD_SSL=NO] [-DBUILD_SHARED=YES] [-DCMAKE_INSTALL_PREFIX=/output-directory] ..
+sudo make
 sudo make install
 sudo ldconfig
 make test
@@ -21,6 +25,9 @@ make
 sudo make install
 sudo ldconfig
 cd ../..
+
+rm -rf rapidjson
+git clone https://github.com/Tencent/rapidjson.git
 
 cd /opt/vc/src/hello_pi
 sudo chmod +x rebuild.sh
